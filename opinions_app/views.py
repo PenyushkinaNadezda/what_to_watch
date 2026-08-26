463a427bb6d51c7c503b2bdcd7907f4f5e81e21d
Penyushkina Nadezda <najia00@qq.com>
2026-08-26 20:46:57 +0800

    flask project

--- opinions_app/views.py
+++ opinions_app/views.py
@@ -7,6 +7,14 @@ from .forms import OpinionForm
 from .models import Opinion
 
 
+def random_opinion():
+    quantity = Opinion.query.count()
+    if quantity:
+        offset_value = randrange(quantity)
+        opinion = Opinion.query.offset(offset_value).first()
+        return opinion
+
+
 @app.route('/')
 def index_view():
     quantity = Opinion.query.count()
